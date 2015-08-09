b75b0e20b80ac2a35cc4dfdb041e8a21aed46cf2
kkd927 <kkd927@gmail.com>
2015-08-10 05:52:10 +0900

    Change HTTP method of registered api

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 	post 'users/id_check' => 'home#id_check'
 
 	namespace :api, path: '/', constraints: { subdomain: 'api' } do
-		get '/check/registered'
+		post '/check/registered'
 		post '/check/mailinglist'
 	end
 
