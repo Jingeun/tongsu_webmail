927586a861250547a0624840438b0b1b4a241d74
kkd927 <kkd927@gmail.com>
2015-08-12 04:44:34 +0900

    Add check channel & group

--- app/controllers/api/mail_controller.rb
+++ app/controllers/api/mail_controller.rb
@@ -11,8 +11,30 @@ module API
 				# If mail isn't registered	
 
 				# Check mail is mailinglist
-				if (!mail.header[:list_id].nil?) || (!mail.header[:list_subsribe]) || (!mail.header[:list_unsubsribe]) || (!mail.header[:list_help])
+				if (!mail.header[:list_id].nil?) || (!mail.header[:list_post])
 					# If mail is mailinglist
+
+					# Get List-Id
+					list_id = ""
+					unless mail.header[:list_id].nil?
+						list_id = mail.header[:list_id].field.value
+						list_id = list_id[/\<(.*)>/,1] || list_id
+					else
+						list_id = mail.header[:list_post].field.value
+						list_id = list_id[/\<(.*)>/,1] || list_id
+						list_id.gsub('mailto:', '')
+					end
+
+					# Check Channel is registered
+					if Mailinglist.find_by(list_id: list_id).present?
+
+					else
+						# Check Group is registered
+						group_name = list_id.split('@').last.split('.')[-2..-1].join('.')
+						if Group.find_by_name(group_name).prersent?
+						else
+						end
+					end
 
 				else
 					# If mail isn't mailinglist
