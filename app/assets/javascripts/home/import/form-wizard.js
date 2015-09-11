a8eca540b0ae04084a5292eff21f86deb5e97d7b
Jingeun <vicious92@naver.com>
2015-09-11 13:39:54 +0900

    Jingeun 2

--- app/assets/javascripts/home/import/form-wizard.js
+++ app/assets/javascripts/home/import/form-wizard.js
@@ -97,12 +97,6 @@ $(function(){
         //causes conflicts when loaded via pjax
         $('.modal.wizard').remove();
         $('.chzn-select').select2();
-        var wizard = $('#satellite-wizard').wizard({
-            keyboard : false,
-            contentHeight : 400,
-            contentWidth : 700,
-            backdrop: 'static'
-        });
 
         $('#fqdn').on('input', function() {
             if ($(this).val().length != 0) {
@@ -150,59 +144,6 @@ $(function(){
                     $this.parents('.form-group').removeClass('has-error').addClass('has-success');
                 }
             }
-        });
-
-        wizard.on('closed', function() {
-            wizard.reset();
-        });
-
-        wizard.on("reset", function() {
-            wizard.modal.find(':input').val('').removeAttr('disabled');
-            wizard.modal.find('.form-group').removeClass('has-error').removeClass('has-succes');
-            wizard.modal.find('#fqdn').data('is-valid', 0).data('lookup', 0);
-        });
-
-        wizard.on("submit", function(wizard) {
-            var submit = {
-                "hostname": $("#new-server-fqdn").val()
-            };
-
-            this.log('seralize()');
-            this.log(this.serialize());
-            this.log('serializeArray()');
-            this.log(this.serializeArray());
-
-            setTimeout(function() {
-                wizard.trigger("success");
-                wizard.hideButtons();
-                wizard._submitting = false;
-                wizard.showSubmitCard("success");
-                wizard.updateProgressBar(0);
-            }, 2000);
-        });
-
-        wizard.el.find(".wizard-success .im-done").click(function() {
-            wizard.hide();
-            setTimeout(function() {
-                wizard.reset();
-            }, 250);
-
-        });
-
-        wizard.el.find(".wizard-success .create-another-server").click(function() {
-            wizard.reset();
-        });
-
-        wizard.el.find('.wizard-progress-container .progress').removeClass('progress-striped')
-            .addClass('progress-xs');
-
-        $(".wizard-group-list").click(function() {
-            alert("Disabled for demo.");
-        });
-
-        $('#open-wizard').click(function(e) {
-            e.preventDefault();
-            wizard.show();
         });
     }
     pageLoad();
